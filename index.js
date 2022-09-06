5a70f8d2902ecfc6db2c0555fbdc14b0eda4bf97
lucieudo.roberto@outlook.com <lucieudo.roberto@outlook.com>
2022-09-06 12:24:15 -0300

    ...

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ function send_mail() {
    
 	
     let user_name = document.getElementById('f-name').value +"%20"+ document.getElementById('l-name').value 
-    let user_mail = document.getElementById('e-mail').value
+    let user_asst = document.getElementById('e-mail').value
     let user_text = document.getElementById('message').value
     
     let mail = {
-    	from : `bcc=${user_mail}CC`,
+    	/*from : `bcc=${user_mail}CC`,*/
     	_for : `mailto:lucieudo.roberto@gmail.com`,
-    	_ass : `subject=${user_name.replaceAll(" ", "%20")}%20%20oportunidade%20de%20trabalho`,
+    	_ass : `subject=olá%20sou%20o%20${user_name.replaceAll(" ", "%20")}%20&${user_asst}`,
     	_msg : `body=${user_text.replaceAll(" ","%20")}`
     }
     
-    window.location.href = `${mail._for}?${mail.from}&${mail._ass}&${mail._msg}`
+    window.location.href = `${mail._for}?${mail._ass}&${mail._msg}`
 }
