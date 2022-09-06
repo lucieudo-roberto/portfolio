0e3cf20168f1503ea5dc05abc6e00cf9f9cae9a7
lucieudo.roberto@outlook.com <lucieudo.roberto@outlook.com>
2022-09-06 12:43:46 -0300

    uncomment mailto url

--- index.js
+++ index.js
@@ -8,13 +8,12 @@ function send_mail() {
     let user_text = document.getElementById('message').value
     
     let mail = {
-    	/*from : `bcc=${user_mail}CC`,*/
+    	//from : `bcc=${user_mail}CC`,
     	_for : `mailto:lucieudo.roberto@gmail.com`,
     	_ass : `subject=olá%20sou%20o%20${user_name.replaceAll(" ", "%20")}%20${user_asst.replaceAll(" ","%20")}`,
     	_msg : `body=${user_text.replaceAll(" ","%20")}`
     }
     
-    console.table(mail)
-    
-    //window.location.href = `${mail._for}?${mail._ass}&${mail._msg}`
+    //console.table(mail)
+    window.location.href = `${mail._for}?${mail._ass}&${mail._msg}`
 }
