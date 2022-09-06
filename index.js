02fd848e09be2f29f5c01ee129eda0f45024e48c
lucieudo.roberto@outlook.com <lucieudo.roberto@outlook.com>
2022-09-06 12:14:36 -0300

    add mail send

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 
 
-function send_main() {
-    let user_name = docuemnt.getElementById('f-name').value + docuemnt.getElementById('l-name').value 
-    let user_mail = docuemnt.getElementById('e-mail').value
-    let user_text = docuemnt.getElementById('message').value
-
-    let mail_object = {
-        
+function send_mail() {
+   
+	
+    let user_name = document.getElementById('f-name').value +"%20"+ document.getElementById('l-name').value 
+    let user_mail = document.getElementById('e-mail').value
+    let user_text = document.getElementById('message').value
+    
+    let mail = {
+    	from : `bcc=${user_mail}CC`,
+    	_for : `mailto:lucieudo.roberto@gmail.com`,
+    	_ass : `subject=${user_name.replaceAll(" ", "%20")}%20%20oportunidade%20de%20trabalho`,
+    	_msg : `body=${user_text.replaceAll(" ","%20")}`
     }
+    
+    window.location.href = `${mail._for}?${mail.from}&${mail._ass}&${mail._msg}`
 }
