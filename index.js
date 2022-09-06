4f0c3d06fed79e8c3f1cd17c69ba0707167cfdd7
lucieudo-roberto <lucieudo.roberto@gmail.com>
2022-09-06 11:12:57 +0000

    ..

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+function send_main() {
+    let user_name = docuemnt.getElementById('f-name').value + docuemnt.getElementById('l-name').value 
+    let user_mail = docuemnt.getElementById('e-mail').value
+    let user_text = docuemnt.getElementById('message').value
+
+    let mail_object = {
+        
+    }
+}
